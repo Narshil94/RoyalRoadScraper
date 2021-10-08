776272bdfc4ee6cab869e8a72c0f50d4a22bfddc
Narshil <narshil.vaghjiani@hyster-yale.com>
2021-10-08 22:02:32 +0100

    Added HTML parser

---
+++ HTMLScraper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace HTMLScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.royalroad.com/fiction/41033/kairos-a-greek-myth-litrpg/chapter/642057/1-the-legend-of-kairos");
+
+            var nodes = doc.DocumentNode.SelectNodes("//a[@class='btn btn-primary col-xs-12']");
+            String link = nodes[0].GetAttributeValue("href",null);
+        }
+    }
+}
