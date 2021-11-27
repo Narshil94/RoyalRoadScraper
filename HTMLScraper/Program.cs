d0b3c0796d9d6328d5aba28b77f9e47cc6db0836
Narshil <narshil.vaghjiani@hyster-yale.com>
2021-11-27 20:55:54 +0000

    Added Chapter object

--- HTMLScraper/Program.cs
+++ HTMLScraper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using HtmlAgilityPack;
 
 namespace HTMLScraper
@@ -9,19 +10,29 @@ namespace HTMLScraper
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            IndexFetcher ifetcher = new IndexFetcher("https://www.royalroad.com/fiction/41033/kairos-a-greek-myth-litrpg/chapter/642057/1-the-legend-of-kairos");
+            string link2chapter1 = "https://www.royalroad.com/fiction/41033/kairos-a-greek-myth-litrpg/chapter/642057/1-the-legend-of-kairos";
+            IndexFetcher ifetcher = new IndexFetcher(link2chapter1);
             ifetcher.getAllChapters();
             var chapters = ifetcher.getChapterList();
-            foreach (var chapter in chapters)
+            foreach (string chapter in chapters)
             {
-                Console.WriteLine(chapter);
+                string chaptername = chapter.Split('/').Last();
                 var web = new HtmlWeb();
                 var doc = web.Load(chapter);
                 var nodes = doc.DocumentNode.SelectNodes($"//div[contains(@class,'chapter-inner chapter-content')]");
-                Console.WriteLine(nodes[0].InnerHtml);
+                string chapterbody  = nodes[0].InnerHtml;
+
+                ifetcher.Chapters.Add(new IndexFetcher.Chapter(chaptername, chapterbody));
+            }
+
+            foreach (var chapter in ifetcher.Chapters)
+            {
+                Console.WriteLine(chapter.ChapterTitle);
+                Console.WriteLine(chapter.ChapterBody);
             }
         }
     }
+
 
     class IndexFetcher
     {
@@ -30,19 +41,20 @@ namespace HTMLScraper
         public IndexFetcher(String chapter1link)
         {
             Chapter1link = chapter1link;
-            Chapters = new List<string>();
+            Chapter_links = new List<string>();
+            Chapters = new List<Chapter>();
         }
 
         public String getNextChapter(String chapterlink)
         {
             web = new HtmlWeb();
             doc = web.Load(chapterlink);
-            Chapters.Add(chapterlink);
+            Chapter_links.Add(chapterlink);
             HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes($"//a[@class='{NextChapterbtn}']");
             int node_idx = 0;
 
             //If its the last chapter exit here
-            if((Chapters.Count > 1) && (nodes.Count == 1))
+            if((Chapter_links.Count > 1) && (nodes.Count == 1))
             {
                 lastChapter = true;
                 return "";
@@ -83,15 +95,29 @@ namespace HTMLScraper
 
         public List<String> getChapterList()
         {
-            return Chapters;
+            return Chapter_links;
         }
+
+
+        public List<Chapter> Chapters;
 
         private HtmlWeb web;
         private HtmlDocument doc;
         private String Chapter1link;
         private String NextChapterbtn = "btn btn-primary col-xs-12";
         private String RRLink = "https://www.royalroad.com";
-        private List<String> Chapters;
+        private List<String> Chapter_links;
         private bool lastChapter = false;
+
+        public class Chapter
+        {
+            public Chapter(string title, string body)
+            {
+                ChapterTitle = title;
+                ChapterBody = body;
+            }
+            public string ChapterTitle;
+            public string ChapterBody;
+        }
     }
 }
